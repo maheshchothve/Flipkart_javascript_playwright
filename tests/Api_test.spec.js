30936d95604a720a8fd0427837bcc1c6b78f6714
mahesh chothve <maheshchothve1997@gmail.com>
2025-10-01 18:08:24 +0530

    api test case

---
+++ tests/Api_test.spec.js
@@ -0,0 +1,8 @@
+import{test,expect} from '@playwright/test'
+
+test('Api testing', async ({ request }) => {
+    const response = await request.get('https://reqres.in/api/users/1');
+    console.log(await response.json()); 
+    //expect(response.status()).toBe(200);
+
+})
