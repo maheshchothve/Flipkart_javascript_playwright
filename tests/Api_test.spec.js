f047a0235b4ceefeef8ce98b8d5a72e2a76f8893
mahesh chothve <maheshchothve1997@gmail.com>
2025-10-09 15:25:17 +0530

    Added Api test cases

--- tests/Api_test.spec.js
+++ tests/Api_test.spec.js
@@ -1,8 +1,17 @@
 import{test,expect} from '@playwright/test'
 
-test('Api testing', async ({ request }) => {
+test.skip('Api testing', async ({ request }) => {
     const response = await request.get('https://reqres.in/api/users/1');
     console.log(await response.json()); 
-    
+    console.log(response.status());
 
 })
+test("get all the products",async({request})=>{
+    const response = await request.get("https://automationexercise.com/api/productsList");
+    const data = await response.json();
+    console.log(data);
+    console.log(response.status());
+    const productNames = data.products.map(p => p.name);
+    expect(productNames).toContain("Blue Top");
+    expect(data.products.find(p => p.name === "Blue Top").price).toBe("Rs. 500");
+});
